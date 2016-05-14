ec9b55dec985676acfae63332a4645f377865cda
xieyunzi <xieyunzi@foxmail.com>
2016-05-14 08:42:56 +0000

    目录读写权限问题等等

--- sample-data/nmap-data.rb
+++ sample-data/nmap-data.rb
@@ -4,11 +4,14 @@
 # usage:
 # LOGSTASH_IP=xxx ./nmap-data.rb
 
-top_sites_filename = 'top_sites.txt'
-logstash_ip = ENV['LOGSTASH_IP'] || `docker-machine ip xyz`
+top_sites_filename = File.dirname(File.expand_path(__FILE__)) + '/top_sites.txt'
+
+puts 'top_sites_filename: ' + top_sites_filename
+puts 'File.exist?(top_sites_filename): ' + File.exist?(top_sites_filename).to_s
 
 File.exist?(top_sites_filename) || `scrapy runspider top_site.py`
+logstash_ip = ENV['LOGSTASH_IP'] || `docker-machine ip xyz`
 
 File.read(top_sites_filename).split.each do |host|
-  `nmap -sV #{host} -oX - | curl -H "x-nmap-target: #{host}" http://#{logstash_ip}:5001 -d @-`
+  puts `nmap -sV #{host} -oX - | curl -H "x-nmap-target: #{host}" http://#{logstash_ip}:5001 -d @-`
 end
